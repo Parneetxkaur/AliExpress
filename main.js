03f2548e5bb2bcb957bfd728c1d87484958fe3ad
Parneetxkaur <115723291+Parneetxkaur@users.noreply.github.com>
2023-04-20 14:52:21 -0400

    a

---
+++ main.js
@@ -0,0 +1,33 @@
+const spaceId = 'egctim46mz60';
+const accessToken = '0tqnhTqjbnO4cBaxV6DKC5EcQD3TgrkEZ5tU9Jt559M';
+
+var gallery = document.getElementById('gallery');
+contentful.createClient({
+  space: spaceId,
+  accessToken: accessToken
+}).getEntries({content_type: 'products'}).then(function(entries) {
+  entries.items.forEach(entry => {
+    console.log(entry);
+    var product = document.createElement('div');
+    product.classList.add('product');
+    gallery.appendChild(product);
+
+    var detailsLink = document.createElement('a');
+    detailsLink.href = 'details.html?id='+entry.sys.id;
+    product.appendChild(detailsLink);
+
+    var galleryImage = document.createElement('img');
+    if (entry.fields.image) {
+      galleryImage.src = 'https:' + entry.fields.image.fields.file.url;
+      console.log(galleryImage.src);
+      galleryImage.classList.add('gallery-image');
+      detailsLink.appendChild(galleryImage);
+    }
+
+  });
+
+}).catch(function(error) {
+  console.log(error);
+});
+
+
